254932e8467358ca12732c87a2eb4d012e650dfb
Greg Allen <me@jga.me>
2013-03-18 15:46:39 -0700

    added --ls to list out instances

--- bin/aws-curl.js
+++ bin/aws-curl.js
@@ -3,14 +3,34 @@
 var rc = require('rc');
 var colors = require('colors');
 var curl = require('../lib/curl');
+var getInstances = require('../lib/get-instances');
 
 
 var opts = rc('awscurl', {
 });
 
+console.log('');
 if (opts.help) {
   console.log('TODO: help message');
   process.exit(0);
+}
+
+if (opts.ls) {
+
+  getInstances(opts, function(err, instances) {
+    instances.forEach(function(instance) {
+      console.log(instance.host.green);
+      console.log('ID:'.grey, instance.id.yellow);
+      console.log('KeyName:'.grey, instance.keyName.yellow);
+      console.log('State:'.grey, instance.state.yellow);
+      console.log('Type:'.grey, instance.type.yellow);
+      console.log('');
+
+    });
+    process.exit(0);
+  });
+
+
 }
 
 curl(opts, function(err, results) {
